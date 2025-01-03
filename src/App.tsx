112338b8e74e65484c9e3dc58560cb8d70af9345
Matteo <matteonegridev@gmail.com>
2025-01-03 22:36:21 +0100

    setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="~text-lg/3xl">Hello</div>;
+  return <div className="m-auto flex p-0 ~text-lg/3xl">Hello</div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,30 @@
-import fluid, {extract} from 'fluid-tailwind';
+import fluid, { extract } from "fluid-tailwind";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  extract,
+  content: { files: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], extract },
   theme: {
-        /** @type {import('fluid-tailwind').FluidThemeConfig} */
-        fluid: ({ theme }) => ({
-          defaultScreens: ['20rem', '96.25rem']
-        }),
-    extend: {},
+    /** @type {import('fluid-tailwind').FluidThemeConfig} */
+    fluid: ({ theme }) => ({
+      defaultScreens: ["20rem", "96.25rem"],
+    }),
+    extend: {
+      colors: {
+        primary: "var(--marine-blue)",
+        secondary: "var(--purplish-blue)",
+        tertiary: "var(--pastel-blue)",
+        quaternary: "var(--light-blue)",
+        error: "var(--strawberry-red)",
+        coolGray: "var(--cool-gray)",
+        lightGray: "var(--light-gray)",
+        magnolia: "var(--magnolia)",
+        alabaster: "var(--alabaster)",
+        white: "var(--white)",
+      },
+    },
   },
-  plugins: [fluid,
+  plugins: [
+    fluid,
     function ({ addBase }) {
       addBase({
         html: {
@@ -21,5 +34,5 @@ export default {
         },
       });
     },
-  ]
+  ],
 };

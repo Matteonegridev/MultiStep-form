c8b537e064718f3e3546a47cb9dbb2b77e58d427
Matteo <matteonegridev@gmail.com>
2025-01-17 21:44:29 +0100

    started working on summary

--- src/components/routes/Summary.tsx
+++ src/components/routes/Summary.tsx
@@ -1,9 +1,55 @@
-import React from "react";
+import { usePlansContext } from "@/hooks/usePlansContext";
+import { useContextHook } from "@/hooks/useStepContext";
+import { SchemaValues } from "@/schema/zodSchema";
 
-type Props = {};
+import { SubmitHandler, useFormContext } from "react-hook-form";
+import { useNavigate } from "react-router";
+import axios from "axios";
+import { Button } from "../ui/button";
 
-function Summary({}: Props) {
-  return <div>Summary</div>;
+function Summary() {
+  const form = useFormContext<SchemaValues>();
+  const navigate = useNavigate();
+  const { isMonthly } = usePlansContext();
+  const { setCurrentStep } = useContextHook();
+
+  const fullData = form.getValues();
+
+  console.log(fullData);
+
+  const sendData = async (formData: SchemaValues) => {
+    try {
+      const response = await axios.post("/api/data", formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("Response data:", response.data);
+
+      return response.data;
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  const onSubmit: SubmitHandler<SchemaValues> = async (data) => {
+    const result = await sendData(data);
+    if (result) {
+      navigate("/thankyou");
+      setCurrentStep((prev) => prev + 1);
+    }
+  };
+
+  return (
+    <form onSubmit={form.handleSubmit(onSubmit)}>
+      <div className="overflow-auto rounded-md bg-gray-100 p-4 text-sm text-gray-800">
+        <p>
+          {fullData.plan.type} <span>({isMonthly ? "monthly" : "yearly"})</span>
+        </p>
+      </div>
+      <Button type="submit">Submit</Button>
+    </form>
+  );
 }
 
 export default Summary;

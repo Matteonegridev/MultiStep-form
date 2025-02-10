2902ef875da32895ab89f5d2c078d04667478a09
Matteo <matteonegridev@gmail.com>
2025-02-10 11:45:07 +0100

    thankyou page desktop done;

--- src/components/routes/Summary.tsx
+++ src/components/routes/Summary.tsx
@@ -11,6 +11,9 @@ import { handlePlanPrice } from "@/functions/selectPlanData";
 import { prevPage } from "@/functions/functions";
 import SvgComp from "@/utilities/SvgComp";
 import mobileSideBar from "/assets/images/bg-sidebar-mobile.svg";
+import desktopSideBar from "/assets/images/bg-sidebar-desktop.svg";
+import Stepper from "../Stepper";
+import { useEffect, useState } from "react";
 
 function Summary() {
   const form = useFormContext<SchemaValues>();
@@ -69,96 +72,147 @@ function Summary() {
     ? totalAddons + parseFloat(plansInfo.replace(/[^-\d]/g, ""))
     : parseFloat(plansInfo.replace(/[^-\d]/g, ""));
 
+  // Resizing useEffect:
+  const mobileSize = window.innerWidth < 768;
+  const [isMobile, setIsMobile] = useState(mobileSize);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <section>
-      <div className="absolute left-0 top-0">
-        <SvgComp alt="mobile sidebar" src={mobileSideBar} width={500} />
-      </div>
-
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="h-[70dvh] bg-magnolia"
+        className="flex h-[calc(90dvh-10px)] flex-col bg-magnolia md:h-screen"
       >
-        <div className="relative z-10 m-auto mt-16 w-11/12 space-y-4 rounded-lg bg-white px-5 py-5 shadow-lg">
-          <div className="w-4/5">
-            <h1 className="pb-2 font-UbuntuBold text-primary ~text-2xl/4xl">
-              Finishing Up
-            </h1>
-            <p className="w-[35ch] font-UbuntuRegular text-coolGray">
-              Double-check everything looks OK before continuing.
-            </p>
-          </div>
-          <div className="overflow-auto rounded-md bg-alabaster p-4 text-sm text-gray-800">
-            <>
-              {/* PLAN AND ADDONS SECTION*/}
-              <div className="flex items-center justify-between border-b-2 border-lightGray pb-2">
-                <div>
-                  <p className="~text-xs/ font-UbuntuBold text-primary">
-                    {fullData.plan.type} <span>(Monthly)</span>
-                  </p>
+        {isMobile && (
+          <>
+            <div className="absolute left-0 top-0">
+              <SvgComp alt="mobile sidebar" src={mobileSideBar} width={780} />
+            </div>
+            <Stepper />
+          </>
+        )}
+        <div className="relative z-10 m-auto mt-16 w-11/12 rounded-lg bg-white px-5 py-8 shadow-lg md:grid md:w-4/5 md:grid-cols-[300px_1fr] md:px-3 md:py-4 xl:w-3/4">
+          {!isMobile && (
+            <div>
+              <SvgComp
+                alt="desktop sidebar"
+                src={desktopSideBar}
+                className="h-auto w-full max-w-[300px] rounded-xl object-cover md:h-full"
+              />
+              <Stepper />
+            </div>
+          )}
+          <div className="space-y-7 md:mx-auto md:w-5/6 md:pt-10 xl:w-2/3">
+            <div className="w-4/5">
+              <h1 className="pb-2 font-UbuntuBold text-primary ~text-2xl/4xl">
+                Finishing Up
+              </h1>
+              <p className="w-[35ch] font-UbuntuRegular text-coolGray">
+                Double-check everything looks OK before continuing.
+              </p>
+            </div>
+            <div className="overflow-auto rounded-md bg-alabaster p-4 text-sm text-gray-800">
+              <>
+                {/* PLAN AND ADDONS SECTION*/}
+                <div className="flex items-center justify-between border-b-2 border-lightGray pb-2">
+                  <div>
+                    <p className="~text-xs/ font-UbuntuBold text-primary">
+                      {fullData.plan.type} <span>(Monthly)</span>
+                    </p>
 
-                  <button
-                    className="text-coolGray underline"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      setCurrentStep((prev) => prev - 2);
-                      navigate("/plan");
-                    }}
-                  >
-                    Change
-                  </button>
+                    <button
+                      className="text-coolGray underline"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setCurrentStep((prev) => prev - 2);
+                        navigate("/plan");
+                      }}
+                    >
+                      Change
+                    </button>
+                  </div>
+                  <p className="font-bold text-primary">{plansInfo}</p>
                 </div>
-                <p className="font-bold text-primary">{plansInfo}</p>
+                <div className="flex flex-col gap-4 pt-4">
+                  {fullData.addons.items && fullData.addons.items.length > 0 ? (
+                    addonsInfo?.map((items, i) => (
+                      <div
+                        key={i}
+                        className="flex items-center justify-between"
+                      >
+                        <p className="font-UbuntuRegular text-coolGray ~text-xs/base">
+                          {items.type}
+                        </p>
+                        <p className="font-UbuntuMedium text-secondary ~text-xs/base">
+                          {items.price}
+                        </p>
+                      </div>
+                    ))
+                  ) : (
+                    <p className="font-semibold text-coolGray">
+                      No add ons selected
+                    </p>
+                  )}
+                </div>
+              </>
+            </div>
+            {/* TOTAL */}
+            <div className="flex items-center justify-between px-3 md:px-6">
+              <p className="font-UbuntuRegular text-coolGray ~text-xs/base">
+                Total ({isMonthly ? "per month" : "per year"})
+              </p>
+              <p className="font-bold text-secondary">
+                ${total}
+                {isMonthly ? "/mo" : "/yr"}
+              </p>
+            </div>
+            {!isMobile && (
+              <div className="flex items-center justify-between py-10">
+                <Button
+                  className="rounded-sm bg-transparent px-[1em] py-[.5em] font-semibold text-coolGray"
+                  onClick={(e) => {
+                    prevPage(setCurrentStep, "/", navigate, e);
+                  }}
+                >
+                  Go Back
+                </Button>
+                <button
+                  className="rounded-sm bg-primary px-[1em] py-[.5em] font-semibold text-white"
+                  type="submit"
+                >
+                  Next Step
+                </button>
               </div>
-              <div className="flex flex-col gap-4 pt-4">
-                {fullData.addons.items && fullData.addons.items.length > 0 ? (
-                  addonsInfo?.map((items, i) => (
-                    <div key={i} className="flex items-center justify-between">
-                      <p className="font-UbuntuRegular text-coolGray ~text-xs/base">
-                        {items.type}
-                      </p>
-                      <p className="font-UbuntuMedium text-secondary ~text-xs/base">
-                        {items.price}
-                      </p>
-                    </div>
-                  ))
-                ) : (
-                  <p className="font-semibold text-coolGray">
-                    No add ons selected
-                  </p>
-                )}
-              </div>
-            </>
-          </div>
-          {/* TOTAL */}
-          <div className="flex items-center justify-between px-3">
-            <p className="font-UbuntuRegular text-coolGray ~text-xs/base">
-              Total ({isMonthly ? "per month" : "per year"})
-            </p>
-            <p className="font-bold text-secondary">
-              ${total}
-              {isMonthly ? "/mo" : "/yr"}
-            </p>
+            )}
           </div>
         </div>
-        <div className="absolute bottom-5 right-4">
-          <Button
-            className="rounded-sm bg-primary px-[1em] py-[.5em] font-semibold text-white"
-            type="submit"
-          >
-            Next Step
-          </Button>
-        </div>
-        <div className="absolute bottom-5 left-4">
-          <Button
-            className="rounded-sm bg-transparent px-[1em] py-[.5em] font-semibold text-coolGray"
-            onClick={(e) => {
-              prevPage(setCurrentStep, "/plan", navigate, e);
-            }}
-          >
-            Go Back
-          </Button>
-        </div>
+        {isMobile && (
+          <>
+            <div className="absolute bottom-5 right-5">
+              <button
+                className="rounded-sm bg-primary px-[1em] py-[.5em] font-semibold text-white"
+                type="submit"
+              >
+                Next Step
+              </button>
+            </div>
+            <div className="absolute bottom-5 left-4">
+              <Button
+                className="rounded-sm bg-transparent px-[1em] py-[.5em] font-semibold text-coolGray"
+                onClick={(e) => {
+                  prevPage(setCurrentStep, "/", navigate, e);
+                }}
+              >
+                Go Back
+              </Button>
+            </div>
+          </>
+        )}
       </form>
     </section>
   );
--- src/components/routes/ThankYou.tsx
+++ src/components/routes/ThankYou.tsx
@@ -1,23 +1,58 @@
 import SvgComp from "@/utilities/SvgComp";
 import thankYou from "/assets/images/icon-thank-you.svg";
 import mobileSideBar from "/assets/images/bg-sidebar-mobile.svg";
+import desktopSideBar from "/assets/images/bg-sidebar-desktop.svg";
+import Stepper from "../Stepper";
+import { useEffect, useState } from "react";
 
 function ThankYou() {
+  // Resizing useEffect:
+  const mobileSize = window.innerWidth < 768;
+  const [isMobile, setIsMobile] = useState(mobileSize);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <section className="min-h-[100dvh] w-full bg-magnolia">
-      <div className="absolute left-0 top-0">
-        <SvgComp alt="mobile sidebar" src={mobileSideBar} width={500} />
-      </div>
-      <div className="relative z-10 m-auto mt-20 flex w-11/12 flex-col items-center rounded-lg bg-white px-3 py-20 shadow-lg">
-        <div className="">
-          <SvgComp alt="Thank you icon" src={thankYou} width={60} height={60} />
+    <section className="min-h-[100dvh] w-full bg-magnolia md:h-screen">
+      {isMobile && (
+        <>
+          <div className="absolute left-0 top-0">
+            <SvgComp alt="mobile sidebar" src={mobileSideBar} width={780} />
+          </div>
+          <Stepper />
+        </>
+      )}
+      <div className="relative z-10 m-auto mt-16 w-11/12 rounded-lg bg-white px-5 py-8 shadow-lg md:grid md:w-4/5 md:grid-cols-[300px_1fr] md:px-3 md:py-4 xl:w-3/4">
+        {!isMobile && (
+          <div>
+            <SvgComp
+              alt="desktop sidebar"
+              src={desktopSideBar}
+              className="h-auto w-full max-w-[300px] rounded-xl object-cover md:h-full"
+            />
+            <Stepper />
+          </div>
+        )}
+
+        <div className="m-auto flex w-4/5 flex-col items-center justify-center rounded-lg bg-white md:gap-4">
+          <SvgComp
+            alt="Thank you icon"
+            src={thankYou}
+            width={60}
+            height={60}
+            className={`${!isMobile && "h-20 w-20"}`}
+          />
+          <h1 className="pt-3 font-bold ~text-2xl/4xl">Thank you!</h1>
+          <p className="text-balance pt-2 text-center font-UbuntuRegular text-coolGray ~text-base/lg">
+            Thanks for confirming your subscription! We hope you have fun using
+            our platform. If you ever need support, please feel free to email us
+            at support@loremgaming.com.
+          </p>
         </div>
-        <h1 className="pt-3 font-bold ~text-2xl/4xl">Thank you!</h1>
-        <p className="text-pretty pt-2 text-center text-coolGray">
-          Thanks for confirming your subscription! We hope you have fun using
-          our platform. If you ever need support, please feel free to email us
-          at support@loremgaming.com.
-        </p>
       </div>
     </section>
   );
